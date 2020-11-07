0aeb081236269f473486bfc350ca4dab0947b6ef
Bjorn Giraud <bjorn.giraud@etu.u-bordeaux.fr>
2020-11-07 16:02:33 +0100

    tests for game_get_expected_nb_tents_*

--- game_test_bjgiraud.c
+++ game_test_bjgiraud.c
@@ -1,17 +1,37 @@
+#include <assert.h>
 #include <stdlib.h>
 #include <stdio.h>
+#include "game.h"
+#include "game_aux.h"
 
-int dummy(char* str1)
+
+int test_game_get_expected_nb_tents_row(cgame g)
 {
-    if (str1 == "dummy")
-    {
-        return EXIT_SUCCESS;
-    }
-    return EXIT_FAILURE;
+    assert ((game_get_expected_nb_tents_row(g,0)) == 3);
 }
 
+///
+
+int test_game_get_expected_nb_tents_all(cgame g)
+{
+    assert(game_get_expected_nb_tents_all(g) == 12);
+}
+
+///
+
+int test_game_get_expected_nb_tents_col(cgame g)
+{
+    assert(game_get_expected_nb_tents_col(g,0) == 4);
+}
+
+///////
+
 int main(int argc, char* argv[]){
-    
-    dummy(argv[1]);
+
+    cgame g1 = game_default();
+
+    test_game_get_expected_nb_tents_row(g1);
+    test_game_get_expected_nb_tents_all(g1);
+    test_game_get_expected_nb_tents_col(g1);
 
 }
