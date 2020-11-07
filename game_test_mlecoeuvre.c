63a2b104ced0b783fd8634f96de48e7c04866267
Lecoeuvre Mathis <mathis.lecoeuvre@etu.u-bordeaux.fr>
2020-11-07 15:56:53 +0100

    Adding tests for game_get_square and game_set_square

--- game_test_mlecoeuvre.c
+++ game_test_mlecoeuvre.c
@@ -1,15 +1,76 @@
 #include <assert.h>
-#include <stdbool.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include "game_aux.h"
 
-bool dummy(char * argv)
-{
-    return EXIT_SUCCESS;
+/* ********** TEST GET_SQUARE ********** */
+bool test_game_get_square(){
+    //default game
+    game g = game_default();
+    //EMPTY square
+    square s = game_get_square(g,0,0);
+    if(s != EMPTY)
+        return false;
+    //TREE square
+    s = game_get_square(g,0,5);
+    if(s != TREE)
+        return false;
+    //TENT square
+    game_set_square(g,0,0,TENT);
+    s = game_get_square(g,0,0);
+    if(s !=TENT ){
+        return false;
+    }
+    //GRASS square
+    game_set_square(g,0,0,GRASS);
+    s = game_get_square(g,0,0);
+    if(s!=GRASS){
+        return false;
+    }
+    return true;
+}
+
+/* ********** TEST SET_SQUARE ********** */
+bool test_game_set_square(){
+    //default game
+    game g = game_default();
+    //EMPTY square
+    game_set_square(g,0,0,EMPTY);
+    square s = game_get_square(g,0,0);
+    if(s != EMPTY)
+        return false;
+    //TENT square
+    game_set_square(g,0,0,TENT);
+    s = game_get_square(g,0,0);
+    if(s !=TENT ){
+        return false;
+    }
+    //GRASS square
+    game_set_square(g,0,0,GRASS);
+    s = game_get_square(g,0,0);
+    if(s!=GRASS){
+        return false;
+    }
+    return true;
 }
 
 int main(int argc, char *argv[])
 {
-    return dummy(argv[1]);
+    bool ok = false;
+    if (strcmp("game_get_square", argv[1]) == 0)
+        ok = test_game_get_square();
+    else if(strcmp("game_set_square", argv[1]) == 0)
+        ok = test_game_set_square();
+    else {
+        fprintf(stderr, "Error: test \"%s\" not found!\n", argv[1]);
+        exit(EXIT_FAILURE);
+    }
+
+    // print test result
+    if (ok) {
+        return EXIT_SUCCESS;
+    } else {
+        return EXIT_FAILURE;
+    }
 }
