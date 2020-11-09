daa1b47f343bd75bfe150cb644e2ed3c300c60dc
Olivier Nang Eko <olivier.nang-eko@etu.u-bordeaux.fr>
2020-11-09 17:06:40 +0100

    Test fonctions Olivier

--- game_test_onangeko.c
+++ game_test_onangeko.c
@@ -1,12 +1,19 @@
 #include <stdio.h>
+#include <assert.h> 
 #include <stdlib.h>
 #include <string.h>
+#include "game.h"
+#include "game_aux.h"
 
 
-char
-test(char *arg)
+
+
+bool
+test_game_is_over()
 {
-    if (strcmp("dummy", arg) == 0)
+    game g1 = game_default();
+    
+    if (game_is_over(g1) == false)
     {
         return EXIT_SUCCESS;
     }
@@ -15,10 +22,93 @@ test(char *arg)
         return EXIT_FAILURE;
     }
 }
-    
+
+
+bool
+test_game_restart()
+{
+    game g1 = game_default();
+    game_play_move(g1, 0,0, TENT);
+    game_restart(g1);
+
+    if (game_get_current_nb_tents_all(g1) == 0 )
+    {
+        return EXIT_SUCCESS;
+    }
+    else
+    {
+        return EXIT_FAILURE;
+    }
+}
+ 
+
+uint test_game_get_current_nb_tents_all ()
+{
+    game g1 = game_default();
+    game_play_move(g1, 0,0, TENT);
+
+    if (game_get_current_nb_tents_all(g1) == 1)
+    {
+        return EXIT_SUCCESS;
+    }
+    return EXIT_FAILURE;
+}
+
+
+uint test_game_get_current_nb_tents_row ()
+{
+
+    game g1 = game_default();
+    game_play_move(g1, 0,0, TENT);
+
+    if (game_get_current_nb_tents_row(g1,0) == 1) 
+    {
+        return EXIT_SUCCESS;
+    }
+    return EXIT_FAILURE;
+
+}
+
+uint test_game_get_current_nb_tents_col ()
+{
+
+    game g1 = game_default();
+    game_play_move(g1, 0,0, TENT);
+
+    if (game_get_current_nb_tents_col(g1,0) == 1) 
+    {
+        return EXIT_SUCCESS;
+    }
+    return EXIT_FAILURE;
+
+}
+
 
 int
-main(int argc, char *argv[])
+main(int argc, char *argv[])    
 {
-    return test(argv[1]);
+      
+
+    fprintf(stderr, "=> Start test \"%s\"\n", argv[1]);
+    if (strcmp("game_is_over", argv[1]) == 0)
+    {
+        return test_game_is_over();
+    }
+    if (strcmp("game_restart", argv[1]) == 0)
+    {
+        return test_game_restart();
+    } 
+    if (strcmp("game_current_tents_all", argv[1]) == 0)
+    {
+        return test_game_get_current_nb_tents_all();
+    }
+    if (strcmp("game_current_tents_row", argv[1]) == 0)
+    {
+        return test_game_get_current_nb_tents_row();
+    }
+    if (strcmp("game_current_tents_col", argv[1]) == 0)
+    {
+        return test_game_get_current_nb_tents_col();
+    } 
+
 }
