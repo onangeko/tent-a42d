837081b6f944ba46032e343b993ff81856f3d3bc
Bjorn Giraud <bjorn.giraud@etu.u-bordeaux.fr>
2020-11-10 15:44:43 +0100

    taking game instead of cgame in argument of tests functions

--- game_test_bjgiraud.c
+++ game_test_bjgiraud.c
@@ -6,28 +6,28 @@
 #include "game_aux.h"
 
 
-void test_game_get_expected_nb_tents_row(cgame g)
+void test_game_get_expected_nb_tents_row(game g)
 {
     assert ((game_get_expected_nb_tents_row(g,0)) == 3);
 }
 
 ///
 
-void test_game_get_expected_nb_tents_all(cgame g)
+void test_game_get_expected_nb_tents_all(game g)
 {
     assert(game_get_expected_nb_tents_all(g) == 12);
 }
 
 ///
 
-void test_game_get_expected_nb_tents_col(cgame g)
+void test_game_get_expected_nb_tents_col(game g)
 {
     assert(game_get_expected_nb_tents_col(g,0) == 4);
 }
 
 ///////
 
-void test_game_set_expected_nb_tents_row(cgame g)
+void test_game_set_expected_nb_tents_row(game g)
 {
     game_set_expected_nb_tents_row(g, 0, 3);
 
@@ -38,7 +38,7 @@ void test_game_set_expected_nb_tents_row(cgame g)
 
 ///
 
-void test_game_set_expected_nb_tents_col(cgame g)
+void test_game_set_expected_nb_tents_col(game g)
 {
     game_set_expected_nb_tents_col(g, 0, 4);
 
@@ -51,7 +51,7 @@ void test_game_set_expected_nb_tents_col(cgame g)
 
 int main(int argc, char* argv[]){
 
-    cgame g1 = game_default();
+    game g1 = game_default();
 
     if (strcmp("game_get_expected_nb_tents_row", argv[1]) == 0)
     {
