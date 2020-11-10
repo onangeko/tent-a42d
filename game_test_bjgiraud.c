f2806fe665c7dbdc5d69faff24ef2641049fd792
Bjorn Giraud <bjorn.giraud@etu.u-bordeaux.fr>
2020-11-10 20:46:27 +0100

    bjgiraud tests works one by one

--- game_test_bjgiraud.c
+++ game_test_bjgiraud.c
@@ -2,57 +2,46 @@
 #include <stdlib.h>
 #include <stdio.h>
 #include <string.h>
-#include "game.h"
 #include "game_aux.h"
 
 
-int test_game_get_expected_nb_tents_row(game g)
+void test_game_get_expected_nb_tents_row(game g)
 {
-    if((game_get_expected_nb_tents_row(g,0)) == 3)
-        return EXIT_SUCCESS;
-    return EXIT_FAILURE;
+    assert((game_get_expected_nb_tents_row(g,0)) == 3);
 }
 
 ///
 
-int test_game_get_expected_nb_tents_all(game g)
+void test_game_get_expected_nb_tents_all(game g)
 {
-    if(game_get_expected_nb_tents_all(g) == 12)
-        return EXIT_SUCCESS;
-    return EXIT_FAILURE;
+    assert(game_get_expected_nb_tents_all(g) == 12);
 }
 
 ///
 
-int test_game_get_expected_nb_tents_col(game g)
+void test_game_get_expected_nb_tents_col(game g)
 {
-    if(game_get_expected_nb_tents_col(g,0) == 4)
-        return EXIT_SUCCESS;
-    return EXIT_FAILURE;
+    assert(game_get_expected_nb_tents_col(g,0) == 4);
 }
 
 ///////
 
-int test_game_set_expected_nb_tents_row(game g)
+void test_game_set_expected_nb_tents_row(game g)
 {
     game_set_expected_nb_tents_row(g, 0, 3);
 
-    if(game_get_expected_nb_tents_row(g,0) == 3)
-        return EXIT_SUCCESS;
-    return EXIT_FAILURE;
+    assert(game_get_expected_nb_tents_row(g,0) == 3);
 
 
 }
 
 ///
 
-int test_game_set_expected_nb_tents_col(game g)
+void test_game_set_expected_nb_tents_col(game g)
 {
     game_set_expected_nb_tents_col(g, 0, 4);
 
-    if(game_get_expected_nb_tents_col(g,0) == 4)
-        return EXIT_SUCCESS;
-    return EXIT_FAILURE;
+    assert(game_get_expected_nb_tents_col(g,0) == 4);
 
 
 }
