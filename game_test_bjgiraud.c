bd71098e118e1ccf5d67f9ccc596cb0288664ac6
Bjorn Giraud <bjorn.giraud@etu.u-bordeaux.fr>
2020-11-10 11:56:42 +0100

    non-void tests functions resolving

--- game_test_bjgiraud.c
+++ game_test_bjgiraud.c
@@ -6,28 +6,28 @@
 #include "game_aux.h"
 
 
-int test_game_get_expected_nb_tents_row(cgame g)
+void test_game_get_expected_nb_tents_row(cgame g)
 {
     assert ((game_get_expected_nb_tents_row(g,0)) == 3);
 }
 
 ///
 
-int test_game_get_expected_nb_tents_all(cgame g)
+void test_game_get_expected_nb_tents_all(cgame g)
 {
     assert(game_get_expected_nb_tents_all(g) == 12);
 }
 
 ///
 
-int test_game_get_expected_nb_tents_col(cgame g)
+void test_game_get_expected_nb_tents_col(cgame g)
 {
     assert(game_get_expected_nb_tents_col(g,0) == 4);
 }
 
 ///////
 
-int test_game_set_expected_nb_tents_row(cgame g)
+void test_game_set_expected_nb_tents_row(cgame g)
 {
     game_set_expected_nb_tents_row(g, 0, 3);
 
@@ -38,7 +38,7 @@ int test_game_set_expected_nb_tents_row(cgame g)
 
 ///
 
-int test_game_set_expected_nb_tents_col(cgame g)
+void test_game_set_expected_nb_tents_col(cgame g)
 {
     game_set_expected_nb_tents_col(g, 0, 4);
 
@@ -55,24 +55,24 @@ int main(int argc, char* argv[]){
 
     if (strcmp("game_get_expected_nb_tents_row", argv[1]) == 0)
     {
-        return test_game_get_expected_nb_tents_row(g1);
+        test_game_get_expected_nb_tents_row(g1);
     }
     if (strcmp("game_get_expected_nb_tents_all", argv[1]) == 0)
     {
-        return test_game_get_expected_nb_tents_all(g1);
+        test_game_get_expected_nb_tents_all(g1);
     }
     if (strcmp("game_get_expected_nb_tents_col", argv[1]) == 0)
     {
-        return test_game_get_expected_nb_tents_col(g1);
+        test_game_get_expected_nb_tents_col(g1);
     }
     
     if (strcmp("game_set_expected_nb_tents_row", argv[1]) == 0)
     {
-        return test_game_set_expected_nb_tents_row(g1);
+        test_game_set_expected_nb_tents_row(g1);
     }
     if (strcmp("game_set_expected_nb_tents_col", argv[1]) == 0)
     {
-        return test_game_set_expected_nb_tents_col(g1);
+        test_game_set_expected_nb_tents_col(g1);
     }
     
     
