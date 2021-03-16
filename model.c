d2f9e11d2b3aca40e92c3db40a6401059af77b31
Lecoeuvre Mathis <mathis.lecoeuvre@etu.u-bordeaux.fr>
2021-03-16 15:54:37 +0100

    init all images

--- model.c
+++ model.c
@@ -8,10 +8,18 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+#define MONKEY "monkey.png"
+#define WATER "water.png"
+#define COCO "coconutTree.png"
+#define SAND "sand.png"
+
 /* **************************************************************** */
 
 struct Env_t {
-  /* PUT YOUR VARIABLES HERE */
+  SDL_Texture* monkey;
+  SDL_Texture* sand;
+  SDL_Texture* water;
+  SDL_Texture* coco;
 };
 
 /* **************************************************************** */
@@ -19,7 +27,21 @@ struct Env_t {
 Env *init(SDL_Window *win, SDL_Renderer *ren, int argc, char *argv[]) {
   Env *env = malloc(sizeof(struct Env_t));
 
-  /* PUT YOUR CODE HERE TO INIT TEXTURES, ... */
+  /* init background texture from PNG image */
+  env->monkey = IMG_LoadTexture(ren, MONKEY)
+  if (!env->monkey) ERROR("IMG_LoadTexture: %s\n", MONKEY);
+
+  /* init background texture from PNG image */
+  env->sand = IMG_LoadTexture(ren, SAND)
+  if (!env->sand) ERROR("IMG_LoadTexture: %s\n", SAND);
+
+  /* init background texture from PNG image */
+  env->coco = IMG_LoadTexture(ren, COCO)
+  if (!env->coco) ERROR("IMG_LoadTexture: %s\n", COCO);
+
+  /* init background texture from PNG image */
+  env->water = IMG_LoadTexture(ren, WATER)
+  if (!env->water) ERROR("IMG_LoadTexture: %s\n", WATER);
 
   return env;
 }
