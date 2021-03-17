ec4ddc69298def4dbfb398c49d2281cf38e6685a
Olivier Nang Eko <olivier.nang-eko@etu.u-bordeaux.fr>
2021-03-17 15:52:59 +0100

    reverting back render()

--- model.c
+++ model.c
@@ -74,11 +74,7 @@ Env *init(SDL_Window *win, SDL_Renderer *ren, int argc, char *argv[]) {
 /* **************************************************************** */
 
 void render(SDL_Window *win, SDL_Renderer *ren, Env *env) { 
-  SDL_Rect rect_monkey;
-  SDL_Rect rect_sand;
-  SDL_Rect rect_water;
-  SDL_Rect rect_coco;
-  SDL_Rect rect_table;
+  SDL_Rect rect;
 
   /* get current window size */
   int w, h;
@@ -88,34 +84,34 @@ void render(SDL_Window *win, SDL_Renderer *ren, Env *env) {
   SDL_RenderCopy(ren, env->background, NULL, NULL); /* stretch it */
 
   /* render table texture */
-  SDL_QueryTexture(env->table, NULL, NULL, &rect_table.w, &rect_table.h);
-  rect_table.x = w / 2 - rect_table.w / 2;
-  rect_table.y = h / 2 - rect_table.h / 2;
-  SDL_RenderCopy(ren, env->table, NULL, &rect_table);
+  SDL_QueryTexture(env->table, NULL, NULL, &rect.w, &rect.h);
+  rect.x = w / 2 - rect.w / 2;
+  rect.y = h / 2 - rect.h / 2;
+  SDL_RenderCopy(ren, env->table, NULL, &rect);
 
   /* render monkey texture */
-  SDL_QueryTexture(env->monkey, NULL, NULL, &rect_monkey.w, &rect_monkey.h);
-  rect_monkey.x = 100;
-  rect_monkey.y = 300;
-  SDL_RenderCopy(ren, env->monkey, NULL, &rect_monkey);
+  SDL_QueryTexture(env->monkey, NULL, NULL, &rect.w, &rect.h);
+  rect.x = 100;
+  rect.y = 100;
+  SDL_RenderCopy(ren, env->monkey, NULL, &rect);
 
   /* render sand texture */
-  SDL_QueryTexture(env->sand, NULL, NULL, &rect_sand.w, &rect_sand.h);
-  rect_sand.x = 10;
-  rect_sand.y = 10;
-  SDL_RenderCopy(ren, env->sand, NULL, &rect_sand);
+  SDL_QueryTexture(env->sand, NULL, NULL, &rect.w, &rect.h);
+  rect.x = 10;
+  rect.y = 10;
+  SDL_RenderCopy(ren, env->sand, NULL, &rect);
 
   /* render coconut texture */
-  SDL_QueryTexture(env->coco, NULL, NULL, &rect_coco.w, &rect_coco.h);
-  rect_coco.x = 120;
-  rect_coco.y = 120;
-  SDL_RenderCopy(ren, env->coco, NULL, &rect_coco);
+  SDL_QueryTexture(env->coco, NULL, NULL, &rect.w, &rect.h);
+  rect.x = 120;
+  rect.y = 120;
+  SDL_RenderCopy(ren, env->coco, NULL, &rect);
 
   /* render water texture */
-  SDL_QueryTexture(env->water, NULL, NULL, &rect_water.w, &rect_water.h);
-  rect_water.x = 300;
-  rect_water.y = 300;
-  SDL_RenderCopy(ren, env->water, NULL, &rect_water);
+  SDL_QueryTexture(env->water, NULL, NULL, &rect.w, &rect.h);
+  rect.x = 300;
+  rect.y = 300;
+  SDL_RenderCopy(ren, env->water, NULL, &rect);
 }
 
 /* **************************************************************** */
